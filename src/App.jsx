648db8cac051092646b7eca069067bbd4ef21a4c
hassanrnt <hassanritouny97@gmail.com>
2023-12-04 14:32:07 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 
 function App() {
+
   function handleClick() {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     console.log(randomNum);
     let userInput = prompt('type a number');
-    alert('Computer number: ${randomNum}, Your guess: ${userInput}');
+    // alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+    alert("Computer number:"+ randomNum + ", Your guess: " + userInput);
   }
-
   return <>
   
   <div>
